8be2a80f368ff790b0a4ecc39398a6b791a091fd
John-Gallardo <johnpatrickgallardo33@gmail.com>
2026-09-12 15:07:40 -0600

    feat: create base template for unique ptr & test default constructor + raw pointer constructor

--- src/UniquePtr.h
+++ src/UniquePtr.h
@@ -2,5 +2,14 @@
 #define UNIQUE_PTR_H
 
 // Your implementation here
+template <typename T>
+class UniquePtr {
+    public:
+        UniquePtr(T *ptr = nullptr) 
+        : m_ptr{ptr} {}
+
+    private:
+        T *m_ptr;
+};
 
 #endif
--- src/main.cpp
+++ src/main.cpp
@@ -3,5 +3,9 @@
 int main() {
     // UniquePtr<int> ptr(new int(5));
     // they test all methods here...
+
+    // 1. test default-initialize & raw pointer constructor
+    UniquePtr<int> ptr(new int(5));
+    UniquePtr<int> ptr2;
     return 0;
 }
