2e7af28636b875025077f0eb4a6488854bed86b4
John-Gallardo <johnpatrickgallardo33@gmail.com>
2026-09-12 15:18:48 -0600

    feat: delete copy semantics & add empty function body for move semantics

--- src/UniquePtr.h
+++ src/UniquePtr.h
@@ -5,8 +5,33 @@
 template <typename T>
 class UniquePtr {
     public:
+        // constructor
         UniquePtr(T *ptr = nullptr) 
         : m_ptr{ptr} {}
+
+        // destructor
+        ~UniquePtr() {
+            if (m_ptr) {
+                delete m_ptr;
+            }
+        }
+
+        // copy constructor isn't allowed
+        UniquePtr(const UniquePtr<T> &uniquePtr) = delete;
+
+        // copy assignment isn't allowed
+        UniquePtr<T> &operator=(const UniquePtr<T> &uniquePtr) = delete;
+
+        // move constructor
+        UniquePtr(UniquePtr<T> &&uniquePtr)
+        : m_ptr{*uniquePtr} {
+            
+        }
+
+        // move assignment
+        UniquePtr<T> &operator=(UniquePtr<T> &&uniquePtr) {
+
+        }
 
     private:
         T *m_ptr;
--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,5 @@
 #include "UniquePtr.h"
+#include <cassert>
 
 int main() {
     // UniquePtr<int> ptr(new int(5));
@@ -7,5 +8,8 @@ int main() {
     // 1. test default-initialize & raw pointer constructor
     UniquePtr<int> ptr(new int(5));
     UniquePtr<int> ptr2;
+
+
+
     return 0;
 }
